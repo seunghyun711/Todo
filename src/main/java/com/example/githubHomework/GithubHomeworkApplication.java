c5c96b89cc11d6ec17aaeef6e2f972dea310dc2a
seunghyun711 <seunghyun711@gmail.com>
2023-04-12 15:59:36 +0900

    init

---
+++ src/main/java/com/example/githubHomework/GithubHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubHomework/GithubHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
